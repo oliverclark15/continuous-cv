afa9f5c2a266e3ab2a69be9b7d50a2a828de3230
Oliver Clark <oliver.clark2@mail.mcgill.ca>
2021-05-12 19:15:13 -0400

    Add files via upload

---
+++ deadlinker.py
@@ -0,0 +1,16 @@
+import re
+import sys
+import requests
+
+
+if __name__ == "__main__":
+	fp = sys.argv[1]
+	bad_links = []
+	with open(fp) as f:
+		try:
+			for x in re.findall('(?:http|ftp|https):\/\/[\w_-]+(?:(?:\.[\w_-]+)+)[\w.,@?^=%&:\/~+#-]*[\w@?^=%&\/~+#-]?', f.read()):
+				if(re.match('404',requests.get(x).text)):
+					bad_links.append(x)
+		except requests.RequestException as e:
+			bad_links.append(x)
+	sys.exit(1 if len(bad_links) != 0 else 0)
